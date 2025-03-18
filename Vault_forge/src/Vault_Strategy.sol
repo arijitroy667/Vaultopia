d9931e3ac27cb899e3a0e42a5861a80c4a42692a
arijitroy667 <arijitroy0445@gmail.com>
2025-03-18 18:50:57 +0530

    contracts updated

--- Vault_forge/src/Vault_Strategy.sol
+++ Vault_forge/src/Vault_Strategy.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./IUniswapV3Pool.sol";
-import "./ICurvePool.sol";
+import "./interfaces/IUniswapV3Pool.sol";
+import "./interfaces/ICurvePool.sol";
 import "./Vault.sol";
 
 contract LiquidityProvisionStrategy is Ownable, ReentrancyGuard {
